d3ebc8c084906d61c0e82561ddee61f5ecae8a06
Martin Ficzel <ficzel@sitegeist.de>
2020-08-20 19:11:09 +0200

    FEATURE: Add IntegratorHelpMessagePostprocessor that renders the name and type for NodeTypes and Properties

---
+++ Classes/NodeTypePostprocessor/IntegratorHelpMessagePostprocessor.php
@@ -0,0 +1,31 @@
+<?php
+namespace Sitegeist\Janitor\NodeTypePostprocessor;
+
+use Neos\ContentRepository\NodeTypePostprocessor\NodeTypePostprocessorInterface;
+use Neos\ContentRepository\Domain\Model\NodeType;
+
+/**
+ * Add default labels and translations to nodetypes and properties
+ */
+class IntegratorHelpMessagePostprocessor implements NodeTypePostprocessorInterface
+{
+
+    /**
+     * @param NodeType $nodeType (uninitialized) The node type to process
+     * @param array $configuration input configuration
+     * @param array $options The processor options
+     * @return void
+     */
+    public function process(NodeType $nodeType, array &$configuration, array $options): void
+    {
+        if (isset($configuration['ui'])) {
+            $configuration['ui']['help']['message'] = 'NodeType: ' . $nodeType->getName();
+        }
+
+        foreach ($configuration['properties'] as $propertyName => &$propertyConfiguration) {
+            if (isset($propertyConfiguration['ui'])) {
+                $propertyConfiguration['ui']['help']['message'] = 'property: ' . $propertyName .' ,type: ' . $propertyConfiguration['type'];
+            }
+        }
+    }
+}
